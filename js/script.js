ff7716be0c5edc53a12476e0c4f21c56ebac15b5
mdev896 <qbert.qwerty.1@gmail.com>
2023-08-11 14:32:51 +0300

    l

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var uprate = 0;
 if(document.cookie != ""){
 	cookies = document.cookie.split("; ")
-	document.getElementById("money").innerHTML = "Money: "+cookies[1].slice(7);
+	document.getElementById("money").innerHTML = "Money: "+cookies[1].slice(6);
 	document.getElementById("up").innerHTML = "Uprate: "+cookies[0].slice(8);
 	uprate = Number(document.getElementById("up").innerHTML.slice(8));
 }else{
