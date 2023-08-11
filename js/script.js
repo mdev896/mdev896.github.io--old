69962b95ad44d2137ebf857b5534d55551cd4515
mdev896 <qbert.qwerty.1@gmail.com>
2023-08-11 14:19:18 +0300

    ll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+var uprate = 0;
 if(document.cookie != ""){
 	cookies = document.cookie.split("; ")
 	document.getElementById("money").innerHTML = "Money: "+cookies[0].slice(7);
 	document.getElementById("up").innerHTML = "Uprate: "+cookies[1].slice(8);
+	uprate = Number(document.getElementById("up").innerHTML.slice(8));
 }
-var uprate = Number(document.getElementById("up").innerHTML.slice(8));
 var dark = true;
 var rate_cost = 100000;
 var seconds = 1200;
